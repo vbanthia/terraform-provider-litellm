eb6f8a9a0ac24265172c8581124bd8bac9b63e1f
Nicholas Cecere <nick@bitop.dev>
2025-01-28 15:31:48 -0500

    fix: unique member add

--- litellm/resource_team_member_add.go
+++ litellm/resource_team_member_add.go
@@ -107,13 +107,76 @@ func resourceLiteLLMTeamMemberAddRead(d *schema.ResourceData, m interface{}) err
 }
 
 func resourceLiteLLMTeamMemberAddUpdate(d *schema.ResourceData, m interface{}) error {
-	// Handle updates by removing all members and adding them back
-	// First delete existing members
-	if err := resourceLiteLLMTeamMemberAddDelete(d, m); err != nil {
-		return err
+	client := m.(*Client)
+	teamID := d.Get("team_id").(string)
+	maxBudget := d.Get("max_budget_in_team").(float64)
+
+	o, n := d.GetChange("member")
+	oldMembers := o.(*schema.Set)
+	newMembers := n.(*schema.Set)
+
+	// Find members to remove (in old but not in new)
+	for _, member := range oldMembers.Difference(newMembers).List() {
+		m := member.(map[string]interface{})
+		deleteData := map[string]interface{}{
+			"team_id": teamID,
+		}
+		if userID, ok := m["user_id"].(string); ok && userID != "" {
+			deleteData["user_id"] = userID
+		}
+		if userEmail, ok := m["user_email"].(string); ok && userEmail != "" {
+			deleteData["user_email"] = userEmail
+		}
+
+		resp, err := MakeRequest(client, "POST", "/team/member_delete", deleteData)
+		if err != nil {
+			return fmt.Errorf("error deleting team member: %v", err)
+		}
+		defer resp.Body.Close()
+
+		if err := handleResponse(resp, "deleting team member"); err != nil {
+			return err
+		}
 	}
-	// Then create new ones
-	return resourceLiteLLMTeamMemberAddCreate(d, m)
+
+	// Find members to add (in new but not in old)
+	membersToAdd := newMembers.Difference(oldMembers).List()
+	if len(membersToAdd) > 0 {
+		membersList := make([]map[string]interface{}, 0, len(membersToAdd))
+		for _, member := range membersToAdd {
+			m := member.(map[string]interface{})
+			memberData := map[string]interface{}{
+				"role": m["role"].(string),
+			}
+			if userID, ok := m["user_id"].(string); ok && userID != "" {
+				memberData["user_id"] = userID
+			}
+			if userEmail, ok := m["user_email"].(string); ok && userEmail != "" {
+				memberData["user_email"] = userEmail
+			}
+			membersList = append(membersList, memberData)
+		}
+
+		memberData := map[string]interface{}{
+			"member":             membersList,
+			"team_id":            teamID,
+			"max_budget_in_team": maxBudget,
+		}
+
+		log.Printf("[DEBUG] Adding new team members request payload: %+v", memberData)
+
+		resp, err := MakeRequest(client, "POST", "/team/member_add", memberData)
+		if err != nil {
+			return fmt.Errorf("error adding team members: %v", err)
+		}
+		defer resp.Body.Close()
+
+		if err := handleResponse(resp, "adding team members"); err != nil {
+			return err
+		}
+	}
+
+	return resourceLiteLLMTeamMemberAddRead(d, m)
 }
 
 func resourceLiteLLMTeamMemberAddDelete(d *schema.ResourceData, m interface{}) error {
